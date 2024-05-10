ec2cb233a215be4fba6d8aa315f476b91381b01c
pc1 <pc1@debian-pc1>
2024-05-10 19:02:49 -0300

    a

--- Parcial-2/Menu.hpp
+++ Parcial-2/Menu.hpp
@@ -9,15 +9,12 @@
 #define MENU_HPP_
 #include <string>
 #include <iostream>
-#include  <map>
+#include <map>
 
 struct Screen
 {
 	void(*action)();
 	std::string text;
-
-	template<typename... Args>
-	Screen(const std::string& text, void(*func)(Args...)) : text(text), action(func) {}
 };
 
 class Menu
@@ -26,9 +23,6 @@ private:
 	std::map<char,Screen> screens;
 public:
 	Menu();
-
-	void AddScreen(const char& key,const std::string& text,  const void (*method));
-
 };
 
 
