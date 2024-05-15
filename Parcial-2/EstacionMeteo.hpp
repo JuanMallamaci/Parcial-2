1f5eeb3d5bffcd198ea675dbebe226cd13617027
aless-94 <168787455+aless-94@users.noreply.github.com>
2024-05-15 12:36:03 -0300

    Update EstacionMeteo.hpp
    Tabs maal no mas

--- Parcial-2/EstacionMeteo.hpp
+++ Parcial-2/EstacionMeteo.hpp
@@ -15,15 +15,15 @@
 #include <string>
 class EstacionMeteo
 {
-	private:
-		std::vector<DatClima> datos;
-	public:
-		EstacionMeteo(){};
-		EstacionMeteo(float vel, float t, float mm, int a, int m, int d, int h, int mn);
-
-		//void SetEstacionMeteo (const std::vector<DatClima>& date) {datos(date);}
-		void SetEstacionMeteo (const DatClima& date) {datos.push_back(date);}
-		EstacionMeteo LecArch(const std::string& ruta , const EstacionMeteo& e);
+private:
+	std::vector<DatClima> datos;
+public:
+	EstacionMeteo(){};
+	EstacionMeteo(float vel, float t, float mm, int a, int m, int d, int h, int mn);
+	
+	//void SetEstacionMeteo (const std::vector<DatClima>& date) {datos(date);}
+	void SetEstacionMeteo (const DatClima& date) {datos.push_back(date);}
+	EstacionMeteo LecArch(const std::string& ruta , const EstacionMeteo& e);
 };
 std::istream& operator>>(std::istream& in,  EstacionMeteo& vec);
 
